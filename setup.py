4db5c154374b89c600e28f856e8f7c738d6cc98b
Rijul Gupta <rijulg@gmail.com>
2024-02-03 15:51:47 +0000

    chore: minor update of keywords

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open("README.md", "r") as readme:
 setuptools.setup(
     name="class-argparse",
     packages=["class_argparse"],
-    version=os.environ['RELEASE_VERSION'],
+    version=os.environ["RELEASE_VERSION"],
     license="MIT",
     description="Class based argument parser",
     author="Neblar",
     author_email="support@neblar.com",
     url="https://github.com/neblar/class-argparse",
-    keywords=["Packaging", "Helper"],
+    keywords=["Class Based", "CLI", "Argparse"],
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
@@ -22,5 +22,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Utilities",
-    ]
+    ],
 )
